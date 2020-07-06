d3dba17dc2f6aef694d4270652456701257e38c6
Mario Merlo <info@kilak.com>
2020-07-06 20:37:18 -0300

    Inicializacion

---
+++ Clase1/leerXMLSimpleXMLElementFile.php
@@ -0,0 +1,17 @@
+<?php
+
+$contenido = new simpleXMLElement('notas.xml', null , true);
+
+if(is_object($contenido)) {
+    for($i = 0; $i < count($contenido->noticia); $i++) {
+      echo "<h1>Título: ".$contenido->noticia[$i]->titulo."</h1>";
+      echo "<p>Cuerpo: ".$contenido->noticia[$i]->cuerpo."</p>";
+      echo "<p>Autor: ".$contenido->noticia[$i]->autor."</p>";
+      echo "<p>Fecha: ".$contenido->noticia[$i]->fecha."</p>";
+      echo "<p>Imagen: ".$contenido->noticia[$i]->imagen."</p>";
+      echo "<hr/>";
+    };
+} else {
+    echo "Ha ocurrido un error";
+}
+
